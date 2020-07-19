2d72cbea41acea57d2c6e11ec5dd6ed468ab6c22
Stephen Clements <mrevilg@gmail.com>
2020-07-19 11:13:14 -0500

    Final Code

--- guess.py
+++ guess.py
@@ -14,3 +14,20 @@ for guessesTaken in range(6):
     print('Take a guess.')
     guess =  input()
     guess = int(guess)
+
+    if guess < number:
+        print('Your guess is too low.')
+
+    if guess > number:
+        print('Your number id too high.')
+
+    if guess == number:
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken + 1)
+    print('Good job ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+
+if guess != number:
+    number = str(number)
+    print('Nope. The number I was thinking of was ' + number + '.')
