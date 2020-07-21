f92c4b951b8de6eb794874efab64ccadec06056d
Stephen Clements <mrevilg@gmail.com>
2020-07-21 06:17:12 -0500

    choose cave

--- dragon.py
+++ dragon.py
@@ -1,4 +1,11 @@
 import random, time
 
 def displayIntro():
-    print(''' You are in a land''')
+    print(''' You are in a land full of dragons. I font of you,
+you see two caves. In one cave, the dragon is friendly
+and will share his treasure with you. The other dragon 
+is greeedy and hungry, and will eat you on sight.''')
+    print()
+
+def chooseCave():
+    cave = ''
