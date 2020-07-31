384fa583e5fccea86069d51a7cd23e434fcae6a4
Stephen Clements <mrevilg@gmail.com>
2020-07-31 06:14:12 -0500

    rand word

--- hangman.py
+++ hangman.py
@@ -43,3 +43,5 @@ words = """ant baboon bat bear beaver camel cat clam cobra cougar
 coyote crow deer dog donkey duck eagle ferret""".split()
 
 def getRandomWord(wordList):
+   wordIndex = random.randint(0, len(wordList) - 1)
+   return wordList[wordIndex]
