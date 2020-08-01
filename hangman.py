ed409a4e9ecde5db278f8dbcc2485423e9fd39a3
Stephen Clements <mrevilg@gmail.com>
2020-08-01 10:52:55 -0500

    more words

--- hangman.py
+++ hangman.py
@@ -40,7 +40,8 @@ HANGMAN_PICS = ['''
     ===''']
 
 words = """ant baboon bat bear beaver camel cat clam cobra cougar 
-coyote crow deer dog donkey duck eagle ferret""".split()
+coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk
+parrot pigeon python""".split()
 
 def getRandomWord(wordList):
    wordIndex = random.randint(0, len(wordList) - 1)
