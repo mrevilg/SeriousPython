801762a271c8948ced0cad0968b3f04a43a3057d
Stephen Clements <mrevilg@gmail.com>
2020-08-03 05:46:31 -0500

    displayBoard

--- hangman.py
+++ hangman.py
@@ -42,10 +42,13 @@ HANGMAN_PICS = ['''
 words = """ant baboon bat bear beaver camel cat clam cobra cougar 
 coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk
 parrot pigeon python lion lizard llama mole monkey moose mouse mule newt otter
-owl panda rabbit ram rat raven rhino salmon seal shark""".split()
+owl panda rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake
+spider stork""".split()
 
 def getRandomWord(wordList):
    wordIndex = random.randint(0, len(wordList) - 1)
    return wordList[wordIndex]
 
 def displayBoard(missedLetters, correctLetters, secretWord):
+   print(HANGMAN_PICS[len(missedLetters)])
+   print
