955f0fdfed295f2c9a4ec201093f67870f0066a2
Stephen Clements <mrevilg@gmail.com>
2020-08-04 06:00:39 -0500

    display missing letter

--- hangman.py
+++ hangman.py
@@ -43,7 +43,7 @@ words = """ant baboon bat bear beaver camel cat clam cobra cougar
 coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk
 parrot pigeon python lion lizard llama mole monkey moose mouse mule newt otter
 owl panda rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake
-spider stork""".split()
+spider stork swan tiger toad trout turkey turtle weasel whale wolf zebra""".split()
 
 def getRandomWord(wordList):
    wordIndex = random.randint(0, len(wordList) - 1)
@@ -52,3 +52,8 @@ def getRandomWord(wordList):
 def displayBoard(missedLetters, correctLetters, secretWord):
    print(HANGMAN_PICS[len(missedLetters)])
    print
+
+   print('Missed letters:', end=' ')
+   for letter in missedLetters:
+      print(letter, end=' ')
+   print()
