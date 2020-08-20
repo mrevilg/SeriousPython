165f7929078074902c6b63047524baf6d98e1245
Stephen Clements <mrevilg@gmail.com>
2020-08-20 06:11:10 -0500

    X or O?

--- tictactoe.py
+++ tictactoe.py
@@ -9,3 +9,13 @@ def drawBoard(board):
     print(board[4] + '|' + board[5] + '|' + board[6])
     print('-+-+-')
     print(board[1] + '|' + board[2] + '|' + board[3])
+
+def inputPlayerLetter():
+    letter = ''
+    while not (letter == 'X' or letter == 'O'):
+        print('Do you want to be X or O?')
+        letter = input().upper()
+        if letter == 'X':
+            return ['X', 'O']
+        else:
+            return ['O', 'X']
