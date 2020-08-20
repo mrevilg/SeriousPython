9f7b1ae3dbb4799c810cea0db45ab82630c2144c
Stephen Clements <mrevilg@gmail.com>
2020-08-19 20:01:14 -0500

    board final

--- tictactoe.py
+++ tictactoe.py
@@ -6,3 +6,6 @@ def drawBoard(board):
 
     print(board[7] + '|' + board[8] + '|' + board[9])
     print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[1] + '|' + board[2] + '|' + board[3])
