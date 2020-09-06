26323ee2cfea1bf4a5c46aca2579e0731a74c968
Stephen Clements <mrevilg@gmail.com>
2020-09-06 10:20:55 -0500

    Secret Number for loop

--- bagels.py
+++ bagels.py
@@ -1,3 +1,12 @@
 import random
+
 NUM_DIGITS = 3
 MAX_GUESS = 10
+
+def getSecretNumber():
+    numbers = list(range(10))
+    random.shuffle(numbers)
+    secretNum = ''
+    for i in range(NUM_DIGITS):
+        secretNum += str(numbers[i])
+    return secretNum
