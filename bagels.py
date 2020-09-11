4f1fc4425dc97d877036511f3028b1d7c11d1453
Stephen Clements <mrevilg@gmail.com>
2020-09-11 14:40:11 -0500

    all clues

--- bagels.py
+++ bagels.py
@@ -19,3 +19,11 @@ def getClues(guess, secretNum):
     for i in range(len(guess)):
         if guess[i] == secretNum[i]:
             clues.append('Fermi')
+        elif guess[i] in secretNum:
+            clues.append('Pico')
+    if len(clues) == 0:
+        return 'Bagels'
+
+    clues.sort()
+    return ' '.join(clues)
+
