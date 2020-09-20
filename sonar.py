e53608acfa71557f12d3e819748474e2245c1aab
Stephen Clements <mrevilg@gmail.com>
2020-09-20 09:30:44 -0500

    board ans waves

--- sonar.py
+++ sonar.py
@@ -5,6 +5,11 @@ import random, sys, math
 def getNewBoard():
     # Create new 60x15 data structure
     board = []
-
-    #gittest
-    #test
+    for x in range(60):
+        board.append([])
+        for y in range(15):
+            if random.randint(0, 1) == 0:
+                board[x].append('~')
+            else:
+                board[x].append('`')
+    return board
