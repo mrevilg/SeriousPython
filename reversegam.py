d246adc09a81b73cefb414421a9502afcb3b1eaa
Stephen Clements <mrevilg@gmail.com>
2020-10-24 08:23:09 -0500

    def getNewBoard

--- reversegam.py
+++ reversegam.py
@@ -10,3 +10,9 @@ def drawBoard(board):
     for y in range(HEIGHT):
         print('%s|' % (y+1), end='')
         for x in range(WIDTH):
+            print(board[x][y], end='')
+        print('%s|' % (y+1))
+    print(' +--------+')    
+    print(' 123456789')
+
+def getNewBoard():
