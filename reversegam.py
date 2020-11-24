04142e90b25ab993def20d1d24e6483ea516e1de
Stephen Clements <mrevilg@gmail.com>
2020-11-24 07:56:26 -0600

    Initial Final Code

--- reversegam.py
+++ reversegam.py
@@ -220,3 +220,14 @@ while True:
     # Display final score.
     drawBoard(finalBoard)
     scores = getScoreOfBoard(finalBoard)
+    print('X scored %s points. O scored %s points.' % (scores['X'], scores['O']))
+    if scores[playerTile] > scores[computerTile]:
+        print('You beat the computer by %s points.' % (scores[playerTile] - scores[computerTile]))
+    elif scores[playerTile] < scores[computerTile]:
+        print('You lost. Computer won by %s points.' % (scores[computerTile] - scores[playerTile]))
+    else:
+        print('The game was a tie!')
+
+    print('Do you want to play again? (yes or no)')
+    if not input().lower().startswith('y'):
+        break
